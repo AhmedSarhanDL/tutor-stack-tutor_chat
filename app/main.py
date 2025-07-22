08ad7da5df29b691d540b7f8da1254294113538c
Ahmed Sarhan <ahmed.sarhan@gmail.com>
2025-07-22 14:33:10 +0300

    feat: test automated root update

--- app/main.py
+++ app/main.py
@@ -9,5 +9,5 @@ class Question(BaseModel):
 
 @app.post("/answer")
 async def answer(q: Question):
-    # Changed response message
-    return {"answer": "this is a good question"} 
+    # Changed response message for the automated PR
+    return {"answer": "this is the automated pull request test"}
